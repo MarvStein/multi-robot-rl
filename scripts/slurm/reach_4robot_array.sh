16b132a43b2fbcfc7325a3133535e5ad7a1dbb0c
Marvin Steinkellner <marvin.k.steinkellner@gmail.com>
2026-06-09 16:08:26 +0200

    increased memory and timeout for 4 robot reach on euler

--- scripts/slurm/reach_4robot_array.sh
+++ scripts/slurm/reach_4robot_array.sh
@@ -3,8 +3,8 @@
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=2G
-#SBATCH --time=07:59:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=11:59:00
 #SBATCH --array=0-9
 #SBATCH --output=scripts/slurm/logs/reach_4robot_%A_%a.out
 #SBATCH --error=scripts/slurm/logs/reach_4robot_%A_%a.err
